d8a7e44537baa625a7f4e0c52c8fd115dd5c130e
kleber.barilli <kleber.barilli73@gmail.com>
2022-10-09 00:44:11 -0300

    feat: bull queue

---
+++ src/bull/queue.js
@@ -0,0 +1,26 @@
+import Bull from 'bull';
+import redisConfig from '../config/redis';
+import * as jobs from './jobs';
+
+const queues = Object.values(jobs).map(job => ({
+    bull: new Bull(job.key, { redis: redisConfig }),
+    name: job.key,
+    handle: job.handle
+}));
+
+export default {
+	queues,
+	add(name, data) {
+		const queue = this.queues.find(queue => queue.name === name);
+		return queue?.bull.add(data, queue.options);
+	},
+	process() {
+		return this.queues.forEach(queue => {
+			queue.bull.process(queue.handle);
+			queue.bull.on("failed", (job, err) => {
+				console.log("Job  failed", queue.name, job.data);
+				console.log(err);
+			});
+		});
+	},
+};
