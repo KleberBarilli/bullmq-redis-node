337fee1a040743484f1e045060e9ffd5f19300e2
kleber.barilli <kleber.barilli73@gmail.com>
2022-10-09 00:30:12 -0300

    chore: aws/redis configs

---
+++ src/config/aws.js
@@ -0,0 +1,6 @@
+export const awsConfig = {
+	apiVersion: "2010-12-01",
+	acessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.AWS_REGION,
+};
