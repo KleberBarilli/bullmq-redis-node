50cb78eeec3891667f1c3414abffbf990a3b127b
kleber.barilli <kleber.barilli73@gmail.com>
2022-10-09 00:53:49 -0300

    chore: add dotenv and queue process

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
+import 'dotenv/config';
 import express from 'express';
+import queue from './bull/queue'; 
 
 const app = express();
+
+queue.process();
 
 app.listen(3333, () => {
     console.log('Server listening on port 3333')
